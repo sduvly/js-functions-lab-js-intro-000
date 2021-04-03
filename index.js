5fc3fecd0b776cb26657be63f3da836ddf5b1329
beautiful-inheritance-1081 <sduvly@gmail.com>
2021-04-03 20:41:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ function happyHolidays(){
   return "Happy holidays!"
 }
 function happyHolidaysTo(name){
-  return `Happy holidays, ${name}`
+  return `Happy holidays, ${name}!`
 }
 function happyCustomHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
